03c5164d5fdb359b50a05716a90b97adb4b4d0a3
khodeprs <parsakhosrawy1384@gmail.com>
2024-11-23 08:19:13 +0330

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-export default ()=>{
-    return "hi"
+import Style from "@/mypage.module.css"
+export default (props:any)=>{
+
+    let a = {color:"red"}
+    return <div className={Style.ali}>
+        {JSON.stringify(props)}
+    </div>
 }
 export const getServerSideProps = async()=>{
     return {
-        props:{}
+        props:{
+            a:{value:"Hi"}
+        }
     }
 }
+
+
